eb41fae3562d92e898847482253e59445648a49d
roihan37 <pro.roihan@gmail.com>
2025-09-10 10:48:24 +0700

    feat: add card in dashboard page

--- absensiku-app/web/app/dashboard/page.tsx
+++ absensiku-app/web/app/dashboard/page.tsx
@@ -23,6 +23,12 @@ import {
   SidebarProvider,
   SidebarTrigger,
 } from "@/components/ui/sidebar"
+import { 
+  UsersRound, 
+  UserRound,
+  CircleArrowRight,
+  ChartNoAxesCombined
+} from 'lucide-react';
 
 export default function Page() {
   return (
@@ -53,22 +59,88 @@ export default function Page() {
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
           <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-            <Card>
+          {/* DATA SISWA */}
+            <Card className="bg-blue-500">
               <CardHeader>
-                <CardTitle className="text-7xl font-bold">12</CardTitle>
-                {/* <p>Data Siswa</p> */}
-                <CardAction>Card Action</CardAction>
+                <CardTitle className="text-7xl font-bold text-zinc-200">12</CardTitle>
+                <p className="font-medium ml-2 text-zinc-200">Data Siswa</p>
+                <CardAction><UsersRound size={114} strokeWidth={1.5} className="text-blue-700" /></CardAction>
               </CardHeader>
-              <CardContent>
-                <p>Data Siswa</p>
-              </CardContent>
-              <CardFooter className="w-full">
-                <div className="flex flex-wrap items-center gap-2 md:flex-row w-full">
-                  <Button>Lihat detail</Button>
+              <CardFooter >
+                <div className="flex flex-wrap w-full items-center gap-2 md:flex-row w-full">
+                  <Button className="w-full bg-blue-700 hover:bg-blue-800">Lihat detail <CircleArrowRight/></Button>
                 </div>
-
               </CardFooter>
+            </Card>
 
+          {/* DATA GURU */}
+            <Card className="bg-emerald-500">
+              <CardHeader>
+                <CardTitle className="text-7xl font-bold text-zinc-200">12</CardTitle>
+                <p className="font-medium ml-2 text-zinc-200">Data Guru</p>
+                <CardAction><UsersRound size={114} strokeWidth={1.5} className="text-emerald-700" /></CardAction>
+              </CardHeader>
+              <CardFooter >
+                <div className="flex flex-wrap w-full items-center gap-2 md:flex-row w-full">
+                  <Button className="w-full bg-emerald-700 hover:bg-emerald-800">Lihat detail <CircleArrowRight/></Button>
+                </div>
+              </CardFooter>
+            </Card>
+
+          {/* DATA ADMIN */}
+            <Card className="bg-yellow-500">
+              <CardHeader>
+                <CardTitle className="text-7xl font-bold text-zinc-200">12</CardTitle>
+                <p className="font-medium ml-2 text-zinc-200">Data Admin</p>
+                <CardAction><UserRound size={114} strokeWidth={1.5} className="text-yellow-700" /></CardAction>
+              </CardHeader>
+              <CardFooter >
+                <div className="flex flex-wrap w-full items-center gap-2 md:flex-row w-full">
+                  <Button className="w-full bg-yellow-700 hover:bg-yellow-800">Lihat detail <CircleArrowRight/></Button>
+                </div>
+              </CardFooter>
+            </Card>
+
+          {/* DATA KELAS */}
+            <Card className="bg-red-500">
+              <CardHeader>
+                <CardTitle className="text-7xl font-bold text-zinc-200">12</CardTitle>
+                <p className="font-medium ml-2 text-zinc-200">Data Kelas</p>
+                <CardAction><CircleArrowRight size={114} strokeWidth={1.5} className="text-red-700" /></CardAction>
+              </CardHeader>
+              <CardFooter >
+                <div className="flex flex-wrap w-full items-center gap-2 md:flex-row w-full">
+                  <Button className="w-full bg-red-700 hover:bg-red-800">Lihat detail <CircleArrowRight/></Button>
+                </div>
+              </CardFooter>
+            </Card>
+
+          {/* DATA TAHUN PELAJARAN */}
+            <Card className="bg-cyan-500">
+              <CardHeader>
+                <CardTitle className="text-7xl font-bold text-zinc-200">12</CardTitle>
+                <p className="font-medium ml-2 text-zinc-200">Data Tahun Pelajaran</p>
+                <CardAction><ChartNoAxesCombined size={114} strokeWidth={1.5} className="text-cyan-700" /></CardAction>
+              </CardHeader>
+              <CardFooter >
+                <div className="flex flex-wrap w-full items-center gap-2 md:flex-row w-full">
+                  <Button className="w-full bg-cyan-700 hover:bg-cyan-800">Lihat detail <CircleArrowRight/></Button>
+                </div>
+              </CardFooter>
+            </Card>
+
+          {/* DATA Profil */}
+            <Card className="bg-sky-500">
+              <CardHeader>
+                <CardTitle className="text-7xl font-bold text-zinc-200">Profil</CardTitle>
+                <p className="font-medium ml-2 text-zinc-200">Profil Saya</p>
+                <CardAction><UserRound size={114} strokeWidth={1.5} className="text-sky-700" /></CardAction>
+              </CardHeader>
+              <CardFooter >
+                <div className="flex flex-wrap w-full items-center gap-2 md:flex-row w-full">
+                  <Button className="w-full bg-sky-700 hover:bg-sky-800">Lihat detail <CircleArrowRight/></Button>
+                </div>
+              </CardFooter>
             </Card>
 
 
