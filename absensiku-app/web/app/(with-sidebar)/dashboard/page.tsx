2e1cc1a1ebcca2ac5bad20f57989aca2e85042cc
roihan37 <pro.roihan@gmail.com>
2025-09-10 11:26:33 +0700

    fix: nav-side bar and route

--- absensiku-app/web/app/(with-sidebar)/dashboard/page.tsx
+++ absensiku-app/web/app/(with-sidebar)/dashboard/page.tsx
@@ -32,33 +32,8 @@ import {
 
 export default function Page() {
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2">
-          <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
-            <Separator
-              orientation="vertical"
-              className="mr-2 data-[orientation=vertical]:h-4"
-            />
-            <Breadcrumb>
-              <BreadcrumbList>
-                <BreadcrumbItem className="hidden md:block">
-                  <BreadcrumbLink href="#">
-                    Building Your Application
-                  </BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbSeparator className="hidden md:block" />
-                <BreadcrumbItem>
-                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
-          </div>
-        </header>
-        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+        < >
+         
           {/* DATA SISWA */}
             <Card className="bg-blue-500">
               <CardHeader>
@@ -145,10 +120,8 @@ export default function Page() {
 
 
 
-          </div>
-          <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
-        </div>
-      </SidebarInset>
-    </SidebarProvider>
+          
+         
+        </>
   )
 }
